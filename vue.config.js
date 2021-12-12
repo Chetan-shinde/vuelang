63b148439e7b8be938b4d56ab80d32b80dbcf68b
unknown <chetanshinde120@gmail.com>
2021-12-12 18:32:25 +0530

    vue sw changes

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
     devServer: {
       disableHostCheck: true
+    },
+    pwa: {
+      workboxPluginMode: 'InjectManifest',
+      workboxOptions: {
+          swSrc: 'src/service-worker.js',
+          exclude: [
+              /\.map$/
+          ],
+      },
+      themeColor: '#1da025'
     }
   }
